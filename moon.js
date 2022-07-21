176f339b3b8db23bb403105cf1579c3d8af45334
unknown <seandanielrooney@gmail.com>
2022-07-20 19:36:44 -0700

    got ascii art and made it relatively responsive + removed code I don't need + figured out how to target phase

--- moon.js
+++ moon.js
@@ -1,11 +1,11 @@
 function load_moon_phases(obj, callback) {
-  var gets = [];
-  for (var i in obj) {
+  const gets = [];
+  for (let i in obj) {
     gets.push(i + "=" + encodeURIComponent(obj[i]));
   }
   gets.push("LDZ=" + new Date(obj.year, obj.month - 1, 1) / 1000);
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&");
+  const xmlhttp = new XMLHttpRequest();
+  const url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&");
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       callback(JSON.parse(xmlhttp.responseText));
@@ -16,9 +16,9 @@ function load_moon_phases(obj, callback) {
 }
 
 function example_1(moon) {
-  var day = new Date().getDate();
-  var dayWeek = moon.phase[day].dayWeek;
-  var html =
+  const day = new Date().getDate();
+  const dayWeek = moon.phase[day].dayWeek;
+  const html =
     "<div>" +
     "<b>" +
     moon.nameDay[dayWeek] +
@@ -30,9 +30,6 @@ function example_1(moon) {
     "</b> " +
     moon.year +
     "</div>" +
-    "<div shadow>" +
-    moon.phase[day].svg +
-    "</div>" +
     "<div>" +
     moon.phase[day].phaseName +
     " " +
@@ -43,14 +40,13 @@ function example_1(moon) {
     "</div>" +
     "</div>";
   document.getElementById("ex1").innerHTML = html;
+  console.log(moon.phase[day].phaseName);
 }
-var configMoon = {
+
+const configMoon = {
   lang: "en",
   month: new Date().getMonth() + 1,
   year: new Date().getFullYear(),
-  size: 150,
-  lightColor: "rgb(255,255,210)",
-  shadeColor: "black",
-  texturize: false,
 };
+
 load_moon_phases(configMoon, example_1);
