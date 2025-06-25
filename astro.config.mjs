67e707a5f25daf1dd19d0173081efe5571c2d948
unknown <seandanielrooney@gmail.com>
2025-06-25 16:20:07 -0700

    converted project to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://asciimoon.com/",
+  integrations: [sitemap()],
+  devToolbar: {
+    enabled: false
+  }
+});
